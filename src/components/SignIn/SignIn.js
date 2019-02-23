38bb1e6ab74cb81fede248606c2a25ca49d5ae1a
Ravi Teja <ravitejabandreddy@gmail.com>
2019-02-24 00:43:27 +0530

    adding front end code

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = ()=>{
-        fetch('http://localhost:3000/signin',{
+        fetch('https://rocky-fjord-71584.herokuapp.com/signin',{
             method:'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
